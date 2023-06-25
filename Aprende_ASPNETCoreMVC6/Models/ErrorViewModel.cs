838df111c87d5588e43ea6a2e14c822b934fa838
Ali <ali.cruz@me.com>
2023-06-24 19:14:07 -0600

    Vista de Registro, se agrega _layout a seccion Usuarios

--- Aprende_ASPNETCoreMVC6/Models/ErrorViewModel.cs
+++ Aprende_ASPNETCoreMVC6/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Aprende_ASPNETCoreMVC6.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
---
+++ Aprende_ASPNETCoreMVC6/Models/RegistroViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aprende_ASPNETCoreMVC6.Models
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico válido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
