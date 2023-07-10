f9c770bf4345a9d8008c65f6fd62549394314c1d
Ali <ali.cruz@me.com>
2023-07-09 18:48:57 -0600

    Migración Roles Video 213

---
+++ Aprende_ASPNETCoreMVC6/Migrations/20230710003617_AdminRol.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aprende_ASPNETCoreMVC6.Migrations
+{
+    public partial class AdminRol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF NOT EXISTS(SELECT Id FROM AspNetRoles 
+                                WHERE Id = '7337f3aa-9917-45f4-ad95-0d05daf80e74')
+                                BEGIN
+                                    INSERT AspNetRoles (Id, [Name], [NormalizedName]) 
+                                    VALUES ('7337f3aa-9917-45f4-ad95-0d05daf80e74', 'admin','ADMIN')
+                                END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles Where Id = '7337f3aa-9917-45f4-ad95-0d05daf80e74'");
+
+        }
+    }
+}
